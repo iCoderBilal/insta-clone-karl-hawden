fdba946e69dd5e131bcad89173e115edbaf0431c
Bilal-12-2 <91077424+iCoderBilal@users.noreply.github.com>
2023-07-24 17:28:05 +0500

    Login Page

--- src/pages/login.js
+++ src/pages/login.js
@@ -19,7 +19,15 @@ export default function Login(){
     })
     
   
-return <p>Hello I am the Login page</p>
+return ( <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+     <div className="flex w-3/5">
+     <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instagram app" />
+     </div>
+
+     <div className="flex flex-col w-2/5">
+      <p>I will be Form</p>
+     </div>
+</div>)
 
 
 }
