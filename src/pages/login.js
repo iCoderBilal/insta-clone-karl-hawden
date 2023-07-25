ad7db120815082472fa306eab0384370b1ba60c7
Bilal-12-2 <91077424+iCoderBilal@users.noreply.github.com>
2023-07-25 17:17:54 +0500

    Working on login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -9,7 +9,9 @@ export default function Login(){
     const [emailAddress, setEmailAddress] = useState('');
     const [password, setPassword] = useState('');
     const [error,setError] = useState("");
-    const isInValid = password === " "  || emailAddress ===" ";
+    const isInvalid = password === ""  || emailAddress ==="";
+
+    console.log(emailAddress)
 
     const handleLogin = () => {};
 
@@ -25,7 +27,36 @@ return ( <div className="container flex mx-auto max-w-screen-md items-center h-s
      </div>
 
      <div className="flex flex-col w-2/5">
-      <p>I will be Form</p>
+        <h1 className="flex justify-center w-full">
+            <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
+          </h1>
+
+          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+          <form onSubmit={handleLogin} method="POST">
+            <input
+              aria-label="Enter your email address"
+              type="text"
+              placeholder="Email address"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({target}) => setEmailAddress(target.value)}
+            />
+            <input
+              aria-label="Enter your password"
+              type="password"
+              placeholder="Password"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setPassword(target.value)}
+              value={password}
+            />
+              <button
+              disabled={isInvalid}
+              type="submit"
+              className={`bg-blue-500 text-white w-full rounded h-8 font-bold
+            ${isInvalid && 'opacity-50'}`}
+            >
+              Login
+            </button>
+            </form>
      </div>
 </div>)
 
